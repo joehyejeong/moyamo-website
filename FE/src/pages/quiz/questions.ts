a61f7f24e0fda00bf7b60556b942ca7c770f94c7
joehyejeong <gpwjdwngml@naver.com>
2025-06-28 08:38:34 +0900

    Update questions.ts

--- FE/src/pages/quiz/questions.ts
+++ FE/src/pages/quiz/questions.ts
@@ -1,5 +1,5 @@
 // 개발용 목 데이터 (API 응답 구조에 맞춤)
-const standingGlbUrl = '/src/assets/models/Standing.glb';
+const standingGlbUrl = '/src/assets/victory.glb';
 export const quizMockData = [
   {
     question_id: 1,
